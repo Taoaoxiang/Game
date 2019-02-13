2a063b21741dd30ccf5693b599a7d6588094bd4b
Taoaoxiang <taoaoxi@isu.edu>
2019-02-13 15:49:43 -0700

    Initilize game Blackjack

---
+++ Blackjack/Blackjack.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+
+using namespace std;
+
+class DECK 
+{
+  public:
+    int open(int n) { return n*52; };
+    void shuffle() {}
+    int draw(){ return 1; }
+  private:
+}
+
+
+int main()
+{
+/*    while (true){
+    }
+*/
+    return 0;
+}
