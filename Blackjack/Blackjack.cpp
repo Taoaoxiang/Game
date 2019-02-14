98815619cb35a83dcbd49559cf0cfae8d5150c42
OliverTao <taoaoxi@isu.edu>
2019-02-14 10:48:06 -0700

    add player

--- Blackjack/Blackjack.cpp
+++ Blackjack/Blackjack.cpp
@@ -14,25 +14,74 @@ class DEALER
 public:
     void welcome() { cout << "Hello player!" << endl;}
     void welcome(string player) { cout << "Hello " << player << "!" << endl;}
+	CARD draw(DECK);
+//private:
+
 };
+
+CARD DEALER::draw(DECK deck)
+{
+	CARD card = deck.draw();
+	return card;
+}
+
+class PLAYER
+{
+public:
+	//void stand();
+	//void hit();
+	PLAYER();
+	PLAYER(std::string playerName = "Player");
+	PLAYER(std::string playerName = "Player", long playerMoney = 100000);
+	void defName(std::string playerName = "Player");
+	void defMoney(long playerMoney = 100000);
+private:
+	long playerMoney;
+	std::string playerName;
+};
+
+PLAYER::PLAYER()
+{
+	playerName = "Player";
+	playerMoney = 100000;
+}
+
+PLAYER::PLAYER(std::string pN)
+{
+	playerName = pN;
+}
+
+PLAYER::PLAYER(std::string pN, long pM) 
+{
+	playerName = pN;
+	playerMoney = pM;
+}
+
+void PLAYER::defName(std::string pN) 
+{
+	playerName = pN;
+}
+
+void PLAYER::defMoney(long pM) 
+{ 
+	playerMoney = pM; 
+}
+
+
 
 int main()
 {
     DEALER dealer1;
-    dealer1.welcome();
     dealer1.welcome("millionaire");
     CARD card1(7, 's');
 	DECK deck1(1) ;
-	deck1.tep();
-	deck1.shuffle();
-	cout << "-------------------" << endl;
-	deck1.tep();
-	cout << "Last: " << deck1.getLastCard().front() << endl;
-	deck1.draw();
-	cout << "Last: " << deck1.getLastCard().front() << endl;
+	
+	
 	//cout << card1.front() << endl;
 /*    while (true){
     }
 */
     return 0;
 }
+
+
